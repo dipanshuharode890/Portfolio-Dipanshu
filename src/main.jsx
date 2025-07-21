240191c2f5960dcc63da55b3e7d123e3e300e8db
dipanshuharode890 <dipanshuharode890.com>
2025-07-21 22:45:42 +0530

    Add vercel.json to fix build error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
@@ -10,9 +9,9 @@ import './styles/global.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-        <HelmetProvider>
+       
             <App />
-        </HelmetProvider> 
+      
     </BrowserRouter>
   </React.StrictMode>
 );
